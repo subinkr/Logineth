dccaed5c951a748120042e6921724b7cb4b9139e
subin <github@subin.kr>
2023-12-24 14:49:20 +0900

    Init: Database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_AWS_HOSTNAME || process.env.DB_HOSTNAME,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+      ssl: process.env.DB_AWS_HOSTNAME && {
+        rejectUnauthorized: false,
+      },
+    }),
+    TypeOrmModule.forFeature([]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

38a9f0e932c3676cdb7301a96e64e065ad1cf773
subin <github@subin.kr>
2023-12-24 14:38:52 +0900

    Init: Data

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,10 +3,11 @@ import { CommonService } from './common.service';
 import { CommonController } from './common.controller';
 import { AuthModule } from './auth/auth.module';
 import { NotiModule } from './noti/noti.module';
+import { DataModule } from './data/data.module';
 
 @Module({
   controllers: [CommonController],
   providers: [CommonService],
-  imports: [AuthModule, NotiModule],
+  imports: [AuthModule, NotiModule, DataModule],
 })
 export class CommonModule {}

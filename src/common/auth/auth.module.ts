0210286d246c3b8673c1b01ceceae8b88058999c
subin <github@subin.kr>
2023-12-24 18:15:19 +0900

    Auth: Use getUserByUsername

--- src/common/auth/auth.module.ts
+++ src/common/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
+import { ProfileService } from 'src/account/profile/profile.service';
 
 @Module({
-  providers: [AuthService, JwtService],
+  providers: [AuthService, JwtService, ProfileService],
 })
 export class AuthModule {}
--- src/source-code/mock/providers/providers.ts
+++ src/source-code/mock/providers/providers.ts
@@ -2,8 +2,12 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { ProfileService } from 'src/account/profile/profile.service';
 import { UserModel } from 'src/source-code/entities/user.entity';
 import { MockUserModel } from '../entities/user.mock';
+import { AuthService } from 'src/common/auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 export const providers = [
+  AuthService,
+  JwtService,
   ProfileService,
   {
     provide: getRepositoryToken(UserModel),

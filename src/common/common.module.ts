6a99e8c9d02210bfc2b734f50fd098fa9bda521b
subin <github@subin.kr>
2023-12-24 14:38:06 +0900

    Init: Auth

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { CommonController } from './common.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   controllers: [CommonController],
   providers: [CommonService],
+  imports: [AuthModule],
 })
 export class CommonModule {}

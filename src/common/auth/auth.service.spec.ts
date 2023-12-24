9928eb992098ba46e601f70ee8d13cb04d197b02
subin <github@subin.kr>
2023-12-24 15:42:31 +0900

    Auth: Make signToken

--- src/common/auth/auth.service.spec.ts
+++ src/common/auth/auth.service.spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { MockUserModel } from '../mock/entities/user.mock';
 
 describe('AuthService', () => {
   let service: AuthService;
+  const user = MockUserModel.user;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,7 +14,12 @@ describe('AuthService', () => {
     service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  // SIGNTOKENTEST: - make, returnx, errorx
+  describe('Sign Token', () => {
+    it('Make | signToken', () => {
+      service.signToken = jest.fn();
+      service.signToken(user.username);
+      expect(service.signToken).toHaveBeenCalledWith(user.username);
+    });
   });
 });

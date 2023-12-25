98db59d00f8a344fc434f2cf0acf8bed4b9a8f75
subin <github@subin.kr>
2023-12-25 12:39:08 +0900

    Fix: Swagger tags

--- src/account/register/register.controller.ts
+++ src/account/register/register.controller.ts
@@ -1,11 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { RegisterService } from './register.service';
 import { ReqLocalRegister } from './dto/req-local-register.dto';
+import { plainToInstance } from 'class-transformer';
+import { ResLocalRegister } from './dto/res-local-register.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('register')
+@ApiTags('account | register')
 export class RegisterController {
   constructor(private readonly registerService: RegisterService) {}
 
   @Post('local')
-  async localRegister(@Body() reqLocalRegister: ReqLocalRegister) {}
+  @ApiOperation({ summary: 'Local register' })
+  async localRegister(@Body() reqLocalRegister: ReqLocalRegister) {
+    const result = await this.registerService.localRegister(reqLocalRegister);
+    return plainToInstance(ResLocalRegister, result);
+  }
 }

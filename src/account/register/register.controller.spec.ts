82ef183c57a4bab593a16e534ab8b0391fb5a558
subin <github@subin.kr>
2023-12-25 12:35:27 +0900

    Register: Add contoller test local register

--- src/account/register/register.controller.spec.ts
+++ src/account/register/register.controller.spec.ts
@@ -1,20 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RegisterController } from './register.controller';
-import { RegisterService } from './register.service';
+import { providers } from 'src/source-code/mock/providers/providers';
+import { MockUserModel } from 'src/source-code/mock/entities/user.mock';
+import { ReqLocalRegister } from './dto/req-local-register.dto';
+import { ResLocalRegister } from './dto/res-local-register.dto';
 
 describe('RegisterController', () => {
   let controller: RegisterController;
+  const { user, accessToken } = MockUserModel;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RegisterController],
-      providers: [RegisterService],
+      providers,
     }).compile();
 
     controller = module.get<RegisterController>(RegisterController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  // LRTEST: - returnx
+  describe('Local Register', () => {
+    it('Return | ResLocalRegister', async () => {
+      const reqLocalRegister: ReqLocalRegister = {};
+      const result = await controller.localRegister(reqLocalRegister);
+      expect(result).toBeInstanceOf(ResLocalRegister);
+
+      const resLocalRegister: ResLocalRegister = { accessToken };
+      const keys = Object.keys(result);
+      const required = Object.keys(resLocalRegister);
+      expect(keys).toEqual(expect.arrayContaining(required));
+    });
   });
 });
--- src/account/register/register.controller.ts
+++ src/account/register/register.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { RegisterService } from './register.service';
+import { ReqLocalRegister } from './dto/req-local-register.dto';
 
 @Controller('register')
 export class RegisterController {
   constructor(private readonly registerService: RegisterService) {}
+
+  @Post('local')
+  async localRegister(@Body() reqLocalRegister: ReqLocalRegister) {}
 }

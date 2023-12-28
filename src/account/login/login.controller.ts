b8f1e86a43afa324d5e3ea7f29dda2d90f549ed5
subin <github@subin.kr>
2023-12-28 14:33:24 +0900

    Login: Make oAuthLogin

--- src/account/login/login.controller.ts
+++ src/account/login/login.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, ParseEnumPipe, Post } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { ReqLocalLogin } from './dto/req-local-login.dto';
 import { plainToInstance } from 'class-transformer';
 import { ResLogin } from './dto/res-login.dto';
 import {
   ApiBadRequestResponse,
+  ApiNotAcceptableResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
@@ -12,6 +13,9 @@ import {
 } from '@nestjs/swagger';
 import { badRequest } from 'src/source-code/error/swagger/bad-request';
 import { notFound } from 'src/source-code/error/swagger/not-found';
+import { ReqOAuthLogin } from './dto/req-oauth-login.dto';
+import { Provider } from 'src/source-code/enum/provider';
+import { notAcceptable } from 'src/source-code/error/swagger/not-acceptable';
 
 @Controller('login')
 @ApiTags('account | login')
@@ -25,6 +29,19 @@ export class LoginController {
   @ApiNotFoundResponse(notFound('유저를 찾을 수 없습니다.'))
   async localLogin(@Body() reqLocalLogin: ReqLocalLogin) {
     const result = await this.loginService.localLogin(reqLocalLogin);
+
+    return plainToInstance(ResLogin, result);
+  }
+
+  @Post('oauth/:provider')
+  @ApiOperation({ summary: 'OAuth login' })
+  @ApiOkResponse({ type: ResLogin })
+  @ApiNotAcceptableResponse(notAcceptable('유저 정보를 가져오지 못했습니다.'))
+  async oAuthLogin(
+    @Param('provider', new ParseEnumPipe(Provider)) provider: Provider,
+    @Body() reqOAuthLogin: ReqOAuthLogin,
+  ) {
+    const result = await this.loginService.oAuthLogin(reqOAuthLogin, provider);
 
     return plainToInstance(ResLogin, result);
   }

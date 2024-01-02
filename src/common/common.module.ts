83774bdee2fc41aaa6977792c6e7f535ad7785f0
subin <github@subin.kr>
2024-01-02 11:14:55 +0900

    Init: WS

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -4,10 +4,11 @@ import { CommonController } from './common.controller';
 import { AuthModule } from './auth/auth.module';
 import { NotiModule } from './noti/noti.module';
 import { DataModule } from './data/data.module';
+import { WsModule } from './ws/ws.module';
 
 @Module({
   controllers: [CommonController],
   providers: [CommonService],
-  imports: [AuthModule, NotiModule, DataModule],
+  imports: [AuthModule, NotiModule, DataModule, WsModule],
 })
 export class CommonModule {}

a9afe8f9b67d41a9609d3ec0c191c4be89c7f8a6
subin <github@subin.kr>
2024-01-08 16:22:10 +0900

    Fix: Import AuthModule

--- src/common/auth/auth.module.ts
+++ src/common/auth/auth.module.ts
@@ -3,11 +3,11 @@ import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModel } from 'src/source-code/entities/user.entity';
-import { ProfileModule } from 'src/account/profile/profile.module';
+import { ProfileService } from 'src/account/profile/profile.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserModel]), ProfileModule],
-  providers: [AuthService, JwtService],
+  imports: [TypeOrmModule.forFeature([UserModel])],
+  providers: [AuthService, JwtService, ProfileService],
   exports: [AuthModule, AuthService],
 })
 export class AuthModule {}

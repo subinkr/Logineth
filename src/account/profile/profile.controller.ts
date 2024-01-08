1127ae343b1d6aab2019c52286b03c67f9267294
subin <github@subin.kr>
2024-01-08 15:54:05 +0900

    Profile: Add swagger and guard

--- src/account/profile/profile.controller.ts
+++ src/account/profile/profile.controller.ts
@@ -5,11 +5,13 @@ import {
   Param,
   ParseIntPipe,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ResGetUserByID } from './dto/res-get-user-by-id.dto';
 import { plainToInstance } from 'class-transformer';
 import {
+  ApiForbiddenResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
@@ -19,6 +21,8 @@ import { notFound } from 'src/source-code/error/swagger/not-found';
 import { ReqEditUser } from './dto/req-edit-user.dto';
 import { ResEditUser } from './dto/res-edit-user.dto';
 import { AuthID } from 'src/common/auth/decorator/id.decorator';
+import { forbidden } from 'src/source-code/error/swagger/forbidden';
+import { AuthGuard } from 'src/common/auth/auth.guard';
 
 @Controller('profile')
 @ApiTags('account | profile')
@@ -37,7 +41,11 @@ export class ProfileController {
   }
 
   @Put(':id/edit')
+  @UseGuards(AuthGuard)
   @ApiOperation({ summary: 'Edit user' })
+  @ApiOkResponse({ type: ResEditUser })
+  @ApiNotFoundResponse(notFound('유저를 찾을 수 없습니다.'))
+  @ApiForbiddenResponse(forbidden('다른 유저를 수정할 수 없습니다.'))
   async editUser(
     @Param('id', ParseIntPipe) targetId: number,
     @Body() reqEditUser: ReqEditUser,

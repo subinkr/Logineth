1d736241e420f5d78119358431d84a8300c79832
subin <github@subin.kr>
2024-01-08 15:36:29 +0900

    Profile: Make editUser controller

--- src/account/profile/profile.controller.ts
+++ src/account/profile/profile.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Put,
+} from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ResGetUserByID } from './dto/res-get-user-by-id.dto';
 import { plainToInstance } from 'class-transformer';
@@ -9,6 +16,9 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { notFound } from 'src/source-code/error/swagger/not-found';
+import { ReqEditUser } from './dto/req-edit-user.dto';
+import { ResEditUser } from './dto/res-edit-user.dto';
+import { AuthID } from 'src/common/auth/decorator/id.decorator';
 
 @Controller('profile')
 @ApiTags('account | profile')
@@ -24,5 +34,20 @@ export class ProfileController {
   ): Promise<ResGetUserByID> {
     const result = await this.profileService.getUserByID(id);
     return plainToInstance(ResGetUserByID, result);
+  }
+
+  @Put(':id/edit')
+  @ApiOperation({ summary: 'Edit user' })
+  async editUser(
+    @Param('id', ParseIntPipe) targetId: number,
+    @Body() reqEditUser: ReqEditUser,
+    @AuthID() id: number,
+  ): Promise<ResEditUser> {
+    const result = await this.profileService.editUser(
+      targetId,
+      reqEditUser,
+      id,
+    );
+    return plainToInstance(ResEditUser, result);
   }
 }

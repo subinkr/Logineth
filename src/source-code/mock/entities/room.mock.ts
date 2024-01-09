33914ac58cb28c0f2b271422b9bf7ae5eb165661
subin <github@subin.kr>
2024-01-09 15:33:35 +0900

    Fix: Set type

--- src/source-code/mock/entities/room.mock.ts
+++ src/source-code/mock/entities/room.mock.ts
@@ -1,13 +1,15 @@
 import { RoomModel } from 'src/source-code/entities/room.entity';
 import { lazyArray } from '../common/lazyArray';
+import { UserModel } from 'src/source-code/entities/user.entity';
+import { ChatModel } from 'src/source-code/entities/chat.entity';
 
 export class MockRoomModel {
   static defaultRoom: RoomModel = {
     id: 1,
     createdAt: new Date(1),
     updatedAt: new Date(1),
-    chats: lazyArray(),
-    users: lazyArray(),
+    chats: lazyArray<ChatModel>(),
+    users: lazyArray<UserModel>(),
   };
 
   static room: RoomModel = {

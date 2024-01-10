bbb313af9d79e9c120225db884e3b46ad3799b85
subin <github@subin.kr>
2024-01-10 15:28:56 +0900

    Friend: Make controller

--- src/account/friend/friend.controller.ts
+++ src/account/friend/friend.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param, Post, UseGuards } from '@nestjs/common';
 import { FriendService } from './friend.service';
+import { AuthID } from 'src/common/auth/decorator/id.decorator';
+import { AuthGuard } from 'src/common/auth/auth.guard';
+import { plainToInstance } from 'class-transformer';
+import { ResFollowing } from './dto/res-following.dto';
+import { ResUnFollowing } from './dto/res-un-following.dto';
 
 @Controller('friend')
 export class FriendController {
   constructor(private readonly friendService: FriendService) {}
+
+  @Post('following/:targetUserID')
+  @UseGuards(AuthGuard)
+  async following(
+    @Param('targetUserID') targetUserID: number,
+    @AuthID() loginUserID: number,
+  ) {
+    const result = await this.friendService.following(
+      targetUserID,
+      loginUserID,
+    );
+    return plainToInstance(ResFollowing, result);
+  }
+
+  @Delete('unFollowing/:targetUserID')
+  @UseGuards(AuthGuard)
+  async unFollowing(
+    @Param('targetUserID') targetUserID: number,
+    @AuthID() loginUserID: number,
+  ) {
+    const result = await this.friendService.unFollowing(
+      targetUserID,
+      loginUserID,
+    );
+    return plainToInstance(ResUnFollowing, result);
+  }
 }

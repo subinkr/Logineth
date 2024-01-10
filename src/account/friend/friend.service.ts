10fb02dc3480aac887504c72a3ad57e1b09803a5
subin <github@subin.kr>
2024-01-10 15:49:08 +0900

    Friend: Solve red lines

--- src/account/friend/friend.service.ts
+++ src/account/friend/friend.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserModel } from 'src/source-code/entities/user.entity';
+import { Repository } from 'typeorm';
+import { ProfileService } from '../profile/profile.service';
 
 @Injectable()
-export class FriendService {}
+export class FriendService {
+  constructor(
+    @InjectRepository(UserModel)
+    private readonly userRepo: Repository<UserModel>,
+    private readonly profileService: ProfileService,
+  ) {}
+
+  async following(targetUserID: number, loginUserID: number) {}
+
+  async unFollowing(targetUserID: number, loginUserID: number) {}
+}

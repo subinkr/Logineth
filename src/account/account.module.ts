fda6ec7a1e6175006eeb906f522e7b99ce61dca8
subin <github@subin.kr>
2024-01-18 10:58:14 +0900

    Init: Setting

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -3,8 +3,9 @@ import { RegisterModule } from './register/register.module';
 import { LoginModule } from './login/login.module';
 import { ProfileModule } from './profile/profile.module';
 import { FriendModule } from './friend/friend.module';
+import { SettingModule } from './setting/setting.module';
 
 @Module({
-  imports: [RegisterModule, LoginModule, ProfileModule, FriendModule],
+  imports: [RegisterModule, LoginModule, ProfileModule, FriendModule, SettingModule],
 })
 export class AccountModule {}

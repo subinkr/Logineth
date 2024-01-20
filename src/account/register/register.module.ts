b7b4cb70feac418c31827a89eaf54ca438f7a87c
subin <github@subin.kr>
2024-01-20 23:07:24 +0900

    Fix: Withdraw bug

--- src/account/register/register.module.ts
+++ src/account/register/register.module.ts
@@ -5,9 +5,14 @@ import { AuthModule } from 'src/common/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModel } from 'src/source-code/entities/user.entity';
 import { ProfileModule } from '../profile/profile.module';
+import { RoomModel } from 'src/source-code/entities/room.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserModel]), AuthModule, ProfileModule],
+  imports: [
+    TypeOrmModule.forFeature([UserModel, RoomModel]),
+    AuthModule,
+    ProfileModule,
+  ],
   controllers: [RegisterController],
   providers: [RegisterService],
   exports: [RegisterService],

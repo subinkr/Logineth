8e3db6f55711d579284b20fed7098761a34c2b89
subin <github@subin.kr>
2024-02-03 15:17:59 +0900

    Make: Connect wallet service

--- src/account/profile/profile.service.spec.ts
+++ src/account/profile/profile.service.spec.ts
@@ -6,6 +6,8 @@ import { ForbiddenException, NotFoundException } from '@nestjs/common';
 import { ReqEditUser } from './dto/req-edit-user.dto';
 import { ResGetUser } from './dto/res-get-user.dto';
 import { ResEditUser } from './dto/res-edit-user.dto';
+import { ReqConnectWallet } from './dto/req-connect-wallet.dto';
+import { ResConnectWallet } from './dto/res-connect-wallet.dto';
 
 describe('ProfileService', () => {
   let service: ProfileService;
@@ -73,6 +75,36 @@ describe('ProfileService', () => {
 
     it('Error | Cannot edit other user.', async () => {
       const result = service.editUser(user.id, reqEditUser, otherUser.id);
+      await expect(result).rejects.toThrow(ForbiddenException);
+    });
+  });
+
+  // CWTEST: - use, return, error
+  describe('Connect Wallet', () => {
+    const reqConnectWallet: ReqConnectWallet = {
+      wallet: user.wallet,
+    };
+    const resConnectWallet: ResConnectWallet = { message: '연결되었습니다.' };
+    let result = {};
+
+    it('Use | getUserByID', async () => {
+      service.getUserByID = jest.fn().mockReturnValue(user);
+      result = await service.connectWallet(user.id, reqConnectWallet, user.id);
+      expect(service.getUserByID).toHaveBeenCalled();
+    });
+
+    it('Return | ResConnectWallet', async () => {
+      const keys = Object.keys(result);
+      const required = Object.keys(resConnectWallet);
+      expect(keys).toEqual(expect.arrayContaining(required));
+    });
+
+    it('Error | Cannot edit other user.', async () => {
+      const result = service.connectWallet(
+        user.id,
+        reqConnectWallet,
+        otherUser.id,
+      );
       await expect(result).rejects.toThrow(ForbiddenException);
     });
   });
